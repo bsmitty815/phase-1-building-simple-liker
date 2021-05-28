a6fba94d3ba39bcd4c199ff7724e89c07bd3fb76
Brian Smith <brian.bubbam@gmail.com>
2021-05-28 13:03:38 -0500

    commit tree f6ef66ce9a172109638ca7b800b6166882de5bad

--- main.js
+++ main.js
@@ -2,7 +2,48 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+  // Your JavaScript code goes here!
+const heart = document.querySelectorAll(".like-glyph"); //set up a for loop so that
+ for (let i=0; i < heart.length; i++) {           // it goes through all the classes
+  heart[i].addEventListener('click', heartSwitch);  // and then when clicked goes to its function
+}
+
+
+    mimicServerCall("fakeurl")
+        .then(data => heartSwitch(data))
+        .catch(function(error) {
+          const modal = document.getElementById('modal')
+          modal.className = "";
+          modal.innerText = error;
+          setTimeout(() =>  modal.className = ".hidden", 3000); //hiding error message
+        });
+
+
+    function heartSwitch(e){ // function to switch the heart
+      //heart.textContent = e.target
+      console.log(e.target.innerHTML) // sets the event target to the innerhtml
+          if (e.target.innerHTML === EMPTY_HEART) { // set to empty
+            e.target.innerHTML = FULL_HEART  // no fill it
+            heart.className = "activated-heart";
+          } else {
+            e.target.innerHTML = EMPTY_HEART // else empty it
+            heart.className = "";
+          }
+      };
+
+  
+    
+
+
+
+
+//event listener click
+// everytime you click a heart the heart fills up
+
+
+
+
+
 
 
 
